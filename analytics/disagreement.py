eaa7fbabaeb382c05de4dcdd3091a5aa359b662c
Natalie Dunn <natalie.dunn@principles.com>
2019-05-01 14:12:10 -0400

    Believable Choice and Output Object Additions

--- analytics/disagreement.py
+++ analytics/disagreement.py
@@ -1,5 +1,8 @@
 
-from src import objects
+import itertools
+import numpy as np
+from src import objects, meta
+from analytics import utils
 
 """
 Core functionality for Disagreement
@@ -81,3 +84,28 @@ def divisiveness(questions: objects.AssertionSet):
         objects.AssertionSet: A set of relevant :class:`objects.Assertion` in a :class:`objects.Context`, or an empty set if none exist.
     """
     pass
+
+
+def believable_choice(question: objects.Question):
+    """
+    INSERT DOCSTRING
+    """
+    total_believability = sum([response.source.believability for response in question.responses.data])
+    # TODO: Terrible factorization using QuestionType
+    categorical_binary = ('CATEGORICAL' in objects.QuestionType.__members__.keys()) | (
+            'BINARY' in objects.QuestionType.__members__.keys())
+    numeric = ('LIKERT' in objects.QuestionType.__members__.keys()) | (
+            'SCALE' in objects.QuestionType.__members__.keys())
+
+    if categorical_binary:
+        return utils.believable_choice_categorical_binary(question, total_believability)
+    elif numeric:
+        return utils.believable_choice_numeric(question, total_believability)
+    else:
+        return None
+
+
+
+
+
+
