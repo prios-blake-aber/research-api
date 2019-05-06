c360e5322f4f432a2acb79952ba92d25b4e3f6a5
Blake Aber <blake.aber@principled.io>
2019-05-06 15:28:42 -0400

    feat: rewriting with dataclasses

--- src/objects.py
+++ src/objects.py
@@ -3,6 +3,9 @@
 TBD
 """
 
+from dataclasses import dataclass, field
+import typing
+
 from src import meta
 from enum import Enum
 
@@ -25,157 +28,6 @@ class QuestionType(Enum):
     RATING = 'Rate This Section'
 
 
-class Judgement(meta.Assertion):
-    """Judgement"""
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-
-class Comparison(meta.Assertion):
-    """Comparison"""
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-
-class Dot(Judgement):
-    """Dot"""
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-
-class Ranking(Judgement):
-    """Ranking"""
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-
-class Response(Judgement):
-    """Response"""
-    # TODO: Index by Person who gave the response along with value.
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-
-class AssertionSet(meta.Entity):
-    """AssertionSet (Useful for Logic)"""
-    def __init__(self, **kwargs):
-        _allowable_collections = {'members': meta.Assertion}
-        super().__init__(allowable_collections=_allowable_collections, **kwargs)
-
-
-class ScaleValueSet(AssertionSet):
-    """ScaleValueSet (Useful for Logic)"""
-    # TODO: add post-init to check all values are floats between 1-10
-    def __init__(self, **kwargs):
-        _allowable_collections = {'members': meta.Assertion}
-        super().__init__(allowable_collections=_allowable_collections, **kwargs)
-
-
-class DotCollection(ScaleValueSet):
-    """Collection of Dots"""
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-
-class ResponseCollection(AssertionSet):
-    """Collection of Responses"""
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-
-class RelevanceCollection(AssertionSet):
-    """Collection of Relevance Scores"""
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-
-class Question(meta.Entity):
-    """Question"""
-    def __init__(self, **kwargs):
-        _allowable_attributes = {'title', 'description', 'question_type'}
-        _allowable_collections = {'responses': Response}
-        super().__init__(_allowable_attributes, _allowable_collections, **kwargs)
-
-
-class Attribute(meta.Entity):
-    """Attribute"""
-    def __init__(self, **kwargs):
-        _allowable_attributes = {'name', 'description'}
-        _allowable_collections = {'members': Attribute}
-        super().__init__(_allowable_attributes, _allowable_collections, **kwargs)
-
-
-class RelevanceScore(meta.Assertion):
-    """RelevanceScore"""
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-    def check_value(self, value):
-        if value not in range(0, 1):
-            raise ValueError('rating must be a float from 0 to 1')
-
-
-class BooleanOption(meta.Assertion):
-    """BooleanOption"""
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-    def check_value(self, value):
-        if (not isinstance(value, bool)) or not None:
-            raise ValueError('Output type must be Boolean or None')
-
-
-class FloatOption(meta.Assertion):
-    """FloatOption"""
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-    def check_value(self, value):
-        if (not isinstance(value, float)) or not None:
-            raise ValueError('Output type must be Float or None')
-
-
-class StringOption(meta.Assertion):
-    """StringOption"""
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-    def check_value(self, value):
-        if (not isinstance(value, str)) or not None:
-            raise ValueError('Output type must be String or None')
-
-
-class System(meta.Entity):
-    """System"""
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-
-class Person(meta.Entity):
-    """Person"""
-    def __init__(self, **kwargs):
-        # TODO: instantiate person object with non-zero believability (e.g. 0.01)
-        _allowable_attributes = {'name', 'role', 'description', 'believability'}
-        _allowable_collections = {'dots': Dot}
-        super().__init__(_allowable_attributes, _allowable_collections, **kwargs)
-
-
-class Team(meta.Entity):
-    """Team"""
-    def __init__(self, **kwargs):
-        _allowable_attributes = {'name', 'description'}
-        _allowable_collections = {'members': Person}
-        super().__init__(_allowable_attributes, _allowable_collections, **kwargs)
-
-
-class Meeting(meta.Entity):
-    """Meeting"""
-    def __init__(self, **kwargs):
-        _allowable_attributes = {'name', 'description'}
-        _allowable_collections = {'dots': Dot, 'questions': Question, 'participants': Person}
-        super().__init__(_allowable_attributes, _allowable_collections, **kwargs)
-
-
 class MeetingNubbyClassification(Enum):
     # TODO: Generalize Classification Class.
     NOT_NUBBY = 1
@@ -183,3 +35,66 @@ class MeetingNubbyClassification(Enum):
     SOMEWHAT_NUBBY = 3
     NUBBY = 4
     VERY_NUBBY = 5
+
+
+@dataclass
+class Judgement(meta.Assertion):
+    description: typing.Optional[str] = 'A Judgement made by one Entity about another Entity'
+
+
+@dataclass
+class Comparison(meta.Assertion):
+    description: typing.Optional[str] = 'A Comparison made between two Entities'
+
+
+@dataclass
+class Dot(Judgement):
+    description: typing.Optional[str] = None
+
+
+@dataclass
+class Ranking(Judgement):
+    description: typing.Optional[str] = None
+
+
+@dataclass
+class Response(Judgement):
+    # TODO: Is this an artifact?
+    description: typing.Optional[str] = None
+
+
+@dataclass
+class Question(meta.Entity):
+    title: typing.Optional[str] = None
+    description: typing.Optional[str] = None
+    question_type: typing.Optional[QuestionType] = None
+    responses: typing.List[Response] = field(default_factory=list)
+
+
+@dataclass
+class System(meta.Entity):
+    description: typing.Optional[str] = None
+
+
+@dataclass
+class Person(meta.Entity):
+    name: typing.Optional[str] = None
+    role: typing.Optional[str] = None
+    description: typing.Optional[str] = None
+    believability: typing.Optional[float] = 0.01
+    dots: typing.List[Dot] = field(default_factory=list)
+
+
+@dataclass
+class Team(meta.Entity):
+    name: typing.Optional[str] = None
+    description: typing.Optional[str] = None
+
+
+@dataclass
+class Meeting(meta.Entity):
+    name: typing.Optional[str] = None
+    description: typing.Optional[str] = None
+    dots: typing.List[Dot] = field(default_factory=list)
+    questions: typing.List[Question] = field(default_factory=list)
+    participants: typing.List[Person] = field(default_factory=list)
